df3f45ed9425c01b69cba7b78b2c2648d5849c13
Sebastian Trujillo <129899570+strujilloa@users.noreply.github.com>
2023-11-30 14:24:59 -0500

    Update hallazgos y empaquetados

--- src/config/global.js
+++ src/config/global.js
@@ -131,13 +131,13 @@ export default {
       },
       {
         icono: 'fas fa-download',
-        titulo: 'Descargar material',
-        download: 'downloads/material.zip',
+        titulo: 'Descargar pdf',
+        download: 'downloads/CFA_06_231100.zip',
       },
       {
         icono: 'fas fa-download',
-        titulo: 'Descargar pdf',
-        download: 'downloads/CFA_06_231100.pdf',
+        titulo: 'Descargar material',
+        download: 'downloads/material.zip',
       },
       {
         icono: 'far fa-registered',
